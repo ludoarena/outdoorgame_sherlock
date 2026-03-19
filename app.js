10e2232828f1e70f3c1611d43b5a3d4d4a073eaa
ludoarena <ludovic.tambour@gmail.com>
2026-03-19 21:19:30 +0100

    Basic code to run server

--- app.js
+++ app.js
@@ -1,1 +1,15 @@
-console.log("Hello work")
+const express = require("express");
+
+const app = express();
+const port = 3000;
+
+app.get("/", (req, res) => res.send("Hello, Express dans local host 2"));
+
+app
+  .listen(port)
+  .on("listening", () => {
+    console.log(`Server running on port ${port}`);
+  })
+  .on("error", (err) => {
+    console.error("Failed to start:", err);
+  });
