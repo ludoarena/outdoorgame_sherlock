5c796f072810e8bc7d916d078c58d8157bdd8306
ludoarena <ludovic.tambour@gmail.com>
2026-03-22 22:32:26 +0100

    API GET /users and /users/:id
    Note : in a first time, data coming from a mock

--- app.js
+++ app.js
@@ -1,10 +1,27 @@
 const express = require("express");
+const { success } = require("./helper.js");
+let USERS = require("./mock-user.js");
 
 const app = express();
 const port = 3000;
 
 app.get("/", (req, res) => res.send("Hello, Express dans local host 2"));
 
+// GET ALL USERS
+app.get("/api/users/", (req, res) => {
+  const message = "The full list of users is found.";
+  res.json(success(message, USERS));
+});
+
+// GET USER by ID
+app.get("/api/users/:id", (req, res) => {
+  const id = parseInt(req.params.id);
+  const user = USERS.find((user) => +user.id === id);
+  const message = "User is found.";
+  res.json(success(message, user));
+});
+
+// launch the server on port 3000
 app
   .listen(port)
   .on("listening", () => {
---
+++ mock-user.js
@@ -0,0 +1,22 @@
+const USERS = [
+  {
+    id: 1,
+    firstName: "Alice",
+    name: "Merveille",
+    email: "alice.merveille@gmail.com",
+  },
+  {
+    id: 2,
+    firstName: "Bernard",
+    name: "Nanard",
+    email: "bernard.nanard@gmail.com",
+  },
+  {
+    id: 3,
+    firstName: "Claude",
+    name: "Sonnet",
+    email: "claude.sonnet@gmail.com",
+  },
+];
+
+module.exports = USERS;
