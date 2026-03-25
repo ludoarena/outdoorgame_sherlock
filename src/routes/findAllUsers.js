78a0822b0afa6da77940c18f2b2c1627ef71d462
ludoarena <ludovic.tambour@gmail.com>
2026-03-25 13:40:58 +0100

    API GET : add filter on last_name

--- src/routes/findAllUsers.js
+++ src/routes/findAllUsers.js
@@ -1,16 +1,29 @@
 const { User } = require("@database/sequelize");
+const { col } = require("sequelize");
 
 module.exports = (app) => {
   app.get("/api/users", (req, res) => {
-    User.findAll()
-      .then((user) => {
-        const message = "TheThe full list of users has been retrieved.";
-        res.json({ message, data: user });
-      })
-      .catch((error) => {
-        const message =
-          "The list of users could not be retrieved. Please try again in a few moments.";
-        res.status(500).json({ message, data: error.message });
+    if (req.query.last_name) {
+      const searchedValue = req.query.last_name;
+      return User.findAll({
+        where: {
+          lastName: searchedValue,
+        },
+      }).then((users) => {
+        const message = `Il y a ${users.length} users qui correspondent au terme de la recherche`;
+        res.json({ message, data: users });
       });
+    } else {
+      User.findAll()
+        .then((users) => {
+          const message = "The full list of users has been retrieved.";
+          res.json({ message, data: users });
+        })
+        .catch((error) => {
+          const message =
+            "The list of users could not be retrieved. Please try again in a few moments.";
+          res.status(500).json({ message, data: error.message });
+        });
+    }
   });
 };
